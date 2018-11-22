586ead1b2d67520f641ec6f5bca66324d254091e
l.boger <l.boger@betzold.de>
2018-11-22 08:37:16 +0100

    added some checkboxes, that we can see that the values are correct, and also edited the main.css

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -33,47 +33,48 @@ export default class App extends Component {
                         </tr>
                         <tr>
                             <td>Master</td>
-                            <td> <input type="text" ref="inputMaster1" /> </td>
-                            <td><input type="text" ref="inputMaster2" /> </td>
-                            <td><input type="text" ref="inputMaster3" /> </td>
-                            <td><input type="text" ref="inputMaster4" /> </td>
-                            <td><input type="text" ref="inputMaster5" /> </td>  
+                            <td><center>X<input type="checkbox" name="checkbox" value="masterPrices" /></center> </td>
+                            <td><center>X<input type="checkbox" name="checkbox" value="masterValuesHtml" /></center> </td>
+                            <td><center>90,2<input type="checkbox" name="checkbox" value="masterValuesFull" /></center> </td>
+                            <td><center>9,7<input type="checkbox" name="checkbox" value="masterValuesAusgelistet" /></center> </td>
+                            <td><center>11,9<input type="checkbox" name="checkbox" value="masterValuesIpoem" /></center> </td>  
                         </tr>
                         <tr>
                             <td>DE</td>
-                            <td><input type="text" ref="inputDE1" /> </td>
-                            <td><input type="text" ref="inputDE2" /> </td>
-                            <td><input type="text" ref="inputDE3" /> </td>
-                            <td><input type="text" ref="inputDE4" /> </td>
-                            <td><input type="text" ref="inputDE5" /> </td> 
+                            <td><center>9,7<input type="checkbox" name="checkbox" value="dePrices" /></center> </td>
+                            <td><center>150<input type="checkbox" name="checkbox" value="deValuesHtml" /></center> </td>
+                            <td><center>29,6<input type="checkbox" name="checkbox" value="deValuesFull" /></center> </td>
+                            <td><center>4,3<input type="checkbox" name="checkbox" value="deValuesAusgelistet" /></center> </td>
+                            <td><center>X<input type="checkbox" name="checkbox" value="deValuesIpoem" /></center> </td>
                         </tr>
                         <tr>
                             <td>AT</td>
-                            <td><input type="text" ref="inputAT1" /> </td>
-                            <td><input type="text" ref="inputAT2" /> </td>
-                            <td><input type="text" ref="inputAT3" /> </td>
-                            <td><input type="text" ref="inputAT4" /> </td>
-                            <td><input type="text" ref="inputAT5" /> </td>  
+                            <td><center>9,1<input type="checkbox" name="checkbox" value="atPrices" /></center> </td>
+                            <td><center>140<input type="checkbox" name="checkbox" value="atValuesHtml" /></center> </td>
+                            <td><center>27,466<input type="checkbox" name="checkbox" value="atValuesFull" /></center> </td>
+                            <td><center>5,1<input type="checkbox" name="checkbox" value="atValuesAusgelistet" /></center> </td>
+                            <td><center>X<input type="checkbox" name="checkbox" value="atValuesIpoem" /></center> </td>
                         </tr>
                         <tr>
                             <td>CH</td>
-                            <td><input type="text" ref="inputCH1" /> </td>
-                            <td><input type="text" ref="inputCH2" /> </td>
-                            <td><input type="text" ref="inputCH3" /> </td>
-                            <td><input type="text" ref="inputCH4" /> </td>
-                            <td><input type="text" ref="inputCH5" /> </td>  
+                            <td><center>9,1<input type="checkbox" name="checkbox" value="chPrices" /></center> </td>
+                            <td><center>138<input type="checkbox" name="checkbox" value="chValuesHtml" /></center> </td>
+                            <td><center>27<input type="checkbox" name="checkbox" value="chValuesFull" /></center> </td>
+                            <td><center>5,1<input type="checkbox" name="checkbox" value="chValuesAusgelistet" /></center> </td>
+                            <td><center>X<input type="checkbox" name="checkbox" value="chValuesIpoem" /></center> </td> 
                         </tr>
                         <tr>
                             <td>EDU</td>
-                            <td><input type="text" ref="inputEDU1" /> </td>
-                            <td><input type="text" ref="inputEDU2" /> </td>
-                            <td><input type="text" ref="inputEDU3" /> </td>
-                            <td><input type="text" ref="inputEDU4" /> </td>
-                            <td><input type="text" ref="inputEDU5" /> </td>  
+                            <td><center>2,9<input type="checkbox" name="checkbox" value="eduPrices" /></center> </td>
+                            <td><center>41<input type="checkbox" name="checkbox" value="eduValuesHtml" /></center> </td>
+                            <td><center>9,7<input type="checkbox" name="checkbox" value="eduValuesFull" /></center> </td>
+                            <td><center>1,6<input type="checkbox" name="checkbox" value="eduValuesAusgelistet" /></center> </td>
+                            <td><center>X<input type="checkbox" name="checkbox" value="eduValuesIpoem" /></center> </td>  
                         </tr>
                         
-                    </table></center>
+                    </table> <h2>Datenstand 11.2018</h2> </center>
             </div>
+            
         );
     }//{this.renderInputs()}
 }
